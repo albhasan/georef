690743ff91dcc28a399cc278a5ff991abb3165f4
alber sánchez <albhasan@gmail.com>
2014-07-01 18:20:09 +0200

    Update Util.js

--- js/georef/Util.js
+++ js/georef/Util.js
@@ -527,9 +527,11 @@ function getStringEscaped(text){
 
 function getLastStringAfterHash(urlTxt){
 	var res = "";
-	var tmpArray = urlTxt.split("#");
-	if(tmpArray.length > 1){
-		res = tmpArray[tmpArray.length - 1];// last element
+	if(urlTxt !== null && urlTxt !== undefined){
+		var tmpArray = urlTxt.split("#");
+		if(tmpArray.length > 1){
+			res = tmpArray[tmpArray.length - 1];// last element
+		}
 	}
 	return res;
 }
@@ -577,7 +579,6 @@ function loadXMLString(txt){
 function removeElement(id){
 	$('#' + id).remove();
 }
-
 
 
 
