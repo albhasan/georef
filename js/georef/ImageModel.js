3edf93b0308786794d41e26e47c805cf94a3760e
alber Sánchez <albhasan@gmail.com>
2014-07-05 16:44:15 +0200

    Files updated from GII-SIIDEMO

--- js/georef/ImageModel.js
+++ js/georef/ImageModel.js
@@ -5,11 +5,6 @@ terms of the Do What The Fuck You Want To Public License, Version 2,
 as published by Sam Hocevar. See the COPYING file for more details.
 */
 
-
-/**
- * Represents an image of a historic map
- *
- */
 function ImageModel(imageUrl, width, height){
 
 	var that = this;
@@ -23,12 +18,7 @@ function ImageModel(imageUrl, width, height){
 	//PRIVATE
 	//---------------------------------------------------------
 
-	/**
-	 * Test if the given point is inside the image boundaries. It uses image's coordinate system
-	 * @param xImg X coordinate in image coordinate system
-	 * @param yImg Y coordinate in image coordinate system
-	 * @returns TRUE if the point fall inside the image, FALSE otherwise
-	 */
+	//Using image's refsys
 	function isInImage(xImg, yImg){
 		res = true;
 		if(xImg < imageOrigin[0] ||  yImg < imageOrigin[1] || xImg > imageWidth || yImg > imageHeight){
@@ -51,41 +41,25 @@ function ImageModel(imageUrl, width, height){
 		return imageHeight;
 	}
 
-	
-	/**
-	 * Returns the minimum coordinates of the image using image's coordinate system
-	 * @returns An single array containing the x and y 
-	 */
+	//Using image's ref sys
 	this.getMinCoords = function(){
 		return imageOrigin;
 	}
-	
-	/**
-	 * Returns the maximum coordinates of the image using image's coordinate system
-	 * @returns An single array containing the x and y 
-	 */
+	//Using image's ref sys
 	this.getMaxCoords = function(){
 		res = new Array();
 		res[0] = this.getWidth();
 		res[1] = this.getHeight();
 		return res;
 	}
-	
-	/**
-	 * Returns the lower left coordinates of the image using image's coordinate system
-	 * @returns An single array containing the x and y 
-	 */
+	//Using image's ref sys
 	this.getLowerLeft = function(){
 		res = new Array();
 		res[0] = this.getMinCoords()[0];
 		res[1] = this.getHeight();
 		return res;
 	}
-
-	/**
-	 * Returns the upper left coordinates of the image using image's coordinate system
-	 * @returns An single array containing the x and y 
-	 */
+	//Using image's ref sys
 	this.getUpperLeft = function(){
 		var res = new Array();
 		var minCoords = this.getMinCoords();
@@ -93,11 +67,7 @@ function ImageModel(imageUrl, width, height){
 		res[1] = minCoords[1];
 		return res;
 	}
-
-	/**
-	 * Returns the upper right coordinates of the image using image's coordinate system
-	 * @returns An single array containing the x and y 
-	 */
+	//Using image's ref sys
 	this.getUpperRight = function(){
 		res = new Array();
 		res[0] = this.getWidth();
@@ -105,10 +75,6 @@ function ImageModel(imageUrl, width, height){
 		return res;
 	}
 
-	/**
-	 * Returns the lower right coordinates of the image using image's coordinate system
-	 * @returns An single array containing the x and y 
-	 */
 	this.getLowerRight = function(){
 		res = new Array();
 		res[0] = this.getWidth();
@@ -116,153 +82,103 @@ function ImageModel(imageUrl, width, height){
 		return res;
 	}
 	
-	/**
-	 * Transforms image to cartesian coordinates assuming the image is in the 4th Quadrant
-	 * @param xImg X coordinate in image coordinate system
-	 * @param yImg Y coordinate in image coordinate system
-	 * @returns An single array containing the x and y transformed coordinates
-	 */
+	//transforms image to cartesian coordinates assuming the image is in the 4th Quadrant
 	this.image2image4q = function(xImg, yImg){
 		return c.image2cartesianCoords(xImg, yImg);
 	}
-	
-	/**
-	 * Transforms image to cartesian coordinates assuming the image is in the 1th Quadrant
-	 * @param xImg X coordinate in image coordinate system
-	 * @param yImg Y coordinate in image coordinate system
-	 * @returns An single array containing the x and y transformed coordinates
-	 */
+	//transforms image to cartesian coordinates assuming the image is in the 1th Quadrant
 	this.image2image1q = function(xImg, yImg){
 		var i4q = this.image2image4q(xImg, yImg);
 		return c.traslation(i4q[0], i4q[1], 0, imageHeight);
 	}
-	
-	
-	/**
-	 * Transforms cartesian to image coordinates assuming the image is in the 4th Quadrant
-	 * @param xImg4q X coordinate in image coordinate system (4th quadrant)
-	 * @param yImg4q Y coordinate in image coordinate system (4th quadrant)
-	 * @returns An single array containing the x and y transformed coordinates
-	 */
+	//transforms cartesian to image coordinates assuming the image is in the 4th Quadrant
 	this.image4q2image = function(xImg4q, yImg4q){
 		return c.cartesian2imageCoords(xImg4q, yImg4q);
 	}
 	
-	/**
-	 * Transforms cartesian to image coordinates assuming the image is in the 1th Quadrant
-	 * @param xImg1q X coordinate in image coordinate system (1st quadrant)
-	 * @param yImg1q Y coordinate in image coordinate system (1st quadrant)
-	 * @returns An single array containing the x and y transformed coordinates
-	 */
+	//transforms cartesian to image coordinates assuming the image is in the 1th Quadrant
 	this.image1q2image = function(xImg1q, yImg1q){
 		var tras = c.traslation(xImg1q, yImg1q, 0, -imageHeight);
 		return c.cartesian2imageCoords(tras[0], tras[1]);
 	}
 	
 	
-	/**
-	 * Returns image's lower left coordinates assuming the image is in the 4th quadrant
-	 * @returns An single array containing the x and y coordinates
-	 */
+	
+	//Assuming the image is in the 4th quadrant
 	this.getCartesianLowerLeft_Image4Q = function(){
 		var imgCoords = this.getLowerLeft();
 		return this.image2image4q(imgCoords[0], imgCoords[1]);
 	}
-	
-	/**
-	 * Returns image's upper left coordinates assuming the image is in the 4th quadrant
-	 * @returns An single array containing the x and y coordinates
-	 */
 	this.getCartesianUpperLeft_Image4Q = function(){
 		var imgCoords = this.getUpperLeft();
 		return this.image2image4q(imgCoords[0], imgCoords[1]);
 	}
-	
-	/**
-	 * Returns image's upper right coordinates assuming the image is in the 4th quadrant
-	 * @returns An single array containing the x and y coordinates
-	 */
+	//Assuming the image is in the 4th quadrant
 	this.getCartesianUpperRight_Image4Q = function(){
 		var imgCoords = this.getUpperRight();
 		return this.image2image4q(imgCoords[0], imgCoords[1]);
 	}
-	
-	/**
-	 * Returns image's lower right coordinates assuming the image is in the 4th quadrant
-	 * @returns An single array containing the x and y coordinates
-	 */
+	//Assuming the image is in the 4th quadrant
 	this.getCartesianLowerRight_Image4Q = function(){
 		var imgCoords = this.getLowerRight();
 		return this.image2image4q(imgCoords[0], imgCoords[1]);
 	}
 	
-
-	/**
-	 * Returns image's lower left coordinates assuming the image is in the 1st quadrant
-	 * @returns An single array containing the x and y coordinates
-	 */
+	//Assuming the image is in the 1st quadrant
 	this.getCartesianLowerLeft_Image1Q = function(){
 		var img = this.getLowerLeft();
 		return this.image2image1q(img[0], img[1]);
 	}
-
-	/**
-	 * Returns image's upper left coordinates assuming the image is in the 1st quadrant
-	 * @returns An single array containing the x and y coordinates
-	 */
+	//Assuming the image is in the 1st quadrant
 	this.getCartesianUpperLeft_Image1Q = function(){
 		var img = this.getUpperLeft();
 		return this.image2image1q(img[0], img[1]);
 	}
-
-	/**
-	 * Returns image's upper right coordinates assuming the image is in the 1st quadrant
-	 * @returns An single array containing the x and y coordinates
-	 */
+	//Assuming the image is in the 1st quadrant
 	this.getCartesianUpperRight_Image1Q = function(){
 		var img = this.getUpperRight();
 		return this.image2image1q(img[0], img[1]);
 	}
-	
-	/**
-	 * Returns image's lower right coordinates assuming the image is in the 1st quadrant
-	 * @returns An single array containing the x and y coordinates
-	 */
+	//Assuming the image is in the 1st quadrant
 	this.getCartesianLowerRight_Image1Q = function(){
 		var img = this.getLowerRight();
 		return this.image2image1q(img[0], img[1]);
 	}
+
 	
-	/**
-	 * Checks of the point is inside the image area using image's coordinate system
-	 * @param xImg X coordinate in image coordinate system
-	 * @param yImg Y coordinate in image coordinate system
-	 * @returns TRUE if the point is inside the image, FALSE otherwise
-	 */
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//Using image's ref sys, checks of the point is inside the image area
 	this.isInsideImage = function(xImg, yImg){
 		return isInImage(xImg, yImg);
 	}
-	
-	/**
-	 * Assuming the image is in the 4th quadrant, checks of the point is inside the image area
-	 * @param xImg4q X coordinate in image coordinate system
-	 * @param yImg4q Y coordinate in image coordinate system
-	 * @returns TRUE if the point is inside the image, FALSE otherwise
-	 */
+	//Assuming the image is in the 4th quadrant, checks of the point is inside the image area
 	this.isInside_Image4q = function(xImg4q, yImg4q){
 		var img = this.image4q2image(xImg4q, yImg4q);
 		return this.isInsideImage(img[0], img[1]);
 	}
-	
-	/**
-	 * Assuming the image is in the 1st quadrant, checks of the point is inside the image area
-	 * @param xImg1q X coordinate in image coordinate system
-	 * @param yImg1q Y coordinate in image coordinate system
-	 * @returns TRUE if the point is inside the image, FALSE otherwise
-	 */
+	//Assuming the image is in the 1st quadrant, checks of the point is inside the image area
 	this.isInside_Image1q = function(xImg1q, yImg1q){
 		var img = this.image1q2image(xImg1q, yImg1q);
 		return this.isInsideImage(img[0], img[1]);
 	}
+	
+	
+
+
+	
 	
 }
--- js/georef/SimilarityTransformation.js
+++ js/georef/SimilarityTransformation.js
@@ -8,7 +8,7 @@ as published by Sam Hocevar. See the COPYING file for more details.
 function SimilarityTransformation(xyArraySource, xyArrayDestination){
 
 	//orthogonal axis
-	//Axes have the same scale (not suitable for georef but for a fist approximation to query DBpedia for suggestions!)
+	//Axes have the same scale (not suitable for georef!)
 
 	
 	var that = this;
@@ -17,7 +17,7 @@ function SimilarityTransformation(xyArraySource, xyArrayDestination){
 	var xyArrayTo;
 	var parameters;
 	
-	if(xyArraySource.length == xyArrayDestination.length && xyArraySource.length > 1){//at least 2 points 
+	if(xyArraySource.length == xyArrayDestination.length && xyArraySource.length > 1){//at least 2 points
 		xyArrayFrom = xyArraySource;
 		xyArrayTo  = xyArrayDestination;
 		parameters = calculateParameters(xyArrayFrom, xyArrayTo);
@@ -29,11 +29,6 @@ function SimilarityTransformation(xyArraySource, xyArrayDestination){
 	//PRIVATE
 	//---------------------------------------------------------
 	
-	/**
-	* Builds the matrix A
-	* @param xyArray - Array made of [x,y] arrays where x and y are numbers
-	* @returns An array resemblig the matrix A for a similarity transformation
-	*/
 	function buildA(xyArray){
 		var res = new Array();
 		for(var i = 0; i < xyArray.length; i++){
@@ -49,11 +44,6 @@ function SimilarityTransformation(xyArraySource, xyArrayDestination){
 		return res;
 	}
 
-	/**
-	* Builds the vector L
-	* @param xyArray - Array made of [x,y] arrays where x and y are numbers
-	* @returns An array resemblig the vector L for a similarity transformation
-	*/
 	function buildL(xyArray){
 		var res = new Array();
 		for(var i = 0; i < xyArray.length; i++){
@@ -67,12 +57,7 @@ function SimilarityTransformation(xyArraySource, xyArrayDestination){
 	}
 
 	
-	/**
-	* Similarity transformation
-	* @param xyArrayFrom - Array made of [x,y] in the image
-	* @param xyArrayTo - Array made of [x,y] in the reference map
-	* @returns An array with the results of the least squares adjustment
-	*/
+	//Similarity transformation
 	function calculateParameters(xyArrayFrom, xyArrayTo){
 		var res = new Array();
 		var Aarray = buildA(xyArrayFrom);
@@ -81,10 +66,6 @@ function SimilarityTransformation(xyArraySource, xyArrayDestination){
 		return res;
 	}
 	
-	/**
-	* Calculates the parameters for reversing the transformation
-	* @returns An array of parameters
-	*/
 	function reverseParameters(){
 		var res = new Array();
 		
@@ -133,11 +114,6 @@ function SimilarityTransformation(xyArraySource, xyArrayDestination){
 		return res;
 	}*/
 
-	/**
-	* Applies the transformation to the input points
-	* @param xyArray - Array made of [x,y] 
-	* @returns An array made of [x,y]  with the resulting coordinates
-	*/
 	this.transform = function(xyArray){
 		var res = new Array();
 		//a0 b0 a b
@@ -173,12 +149,6 @@ function SimilarityTransformation(xyArraySource, xyArrayDestination){
 		return res;
 	}*/
 	
-	
-	/**
-	* Applies the reverse transformation to the input points
-	* @param xyArray - Array made of [x,y] 
-	* @returns An array made of [x,y]  with the resulting coordinates
-	*/
 	this.transformReverse = function(xyArray){
 		var res = new Array();
 		
